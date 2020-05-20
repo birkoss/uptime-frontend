c3c7695f1d6f93f46fb625ad6be2125df5334475
Mathieu Robichaud <mathieu@standish.ca>
2020-05-20 10:28:22 -0400

    Added new BarChart

--- src/components/Endpoint.js
+++ src/components/Endpoint.js
@@ -51,6 +51,8 @@ class Endpoint extends Component {
                                 average: stat['response_time__avg'],
                                 min: stat['response_time__min'],
                                 max: stat['response_time__max'],
+                                count: stat['response_time__count'],
+                                fullDate: stat['grouping'],
                             });
                     });
                 } else {
@@ -75,43 +77,65 @@ class Endpoint extends Component {
             })
     }
 
+    toolTipContent(abc) {
+        if (abc.active) {
+            // Fetch the correct stats
+            let stat = this.state.stats.filter(stat => stat.date === abc.label);
+            if (stat.length !== 1) {
+                return null;
+            }
+
+            return (
+                <div className="custom-tooltip">
+                    <p className="intro">{stat[0].fullDate} ({stat[0].count})</p>
+                    {
+                        abc.payload.map(payload => {
+                            return (
+                                <p key={ payload.name } className="label">{`${payload.name} : ${payload.value}`}</p>
+                            );
+                        })
+                    }
+                </div>
+            );
+        }
+
+        return null;
+    }
+
     render() {
         let colors = ["#8884d8", "#82ca9d", "#82ca9d", "#82ca9d", "#82ca9d", "#82ca9d"];
         
         return (
             <div>
                 <ResponsiveContainer width='100%' minHeight='300px'>
-                    { this.props['apiEndpoint'] === 'stats' ? (
-                    <LineChart data={this.state.stats} margin={{ top: 5, right: 30, left: 20, bottom: 5, }}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="date" />
-                        <YAxis />
-                        <Tooltip />
-                        <Line type="monotone" dataKey="average" stroke="#8884d8" activeDot={{ r: 8 }} />
-                        <Line type="monotone" dataKey="min" stroke="#00ff00" />
-                        <Line type="monotone" dataKey="max" stroke="#ff0000" />
-                    </LineChart>) : (
-      <BarChart
-      data={this.state.stats}
-      margin={{
-        top: 5, right: 30, left: 20, bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      {
-          this.state.codes.map((code, index) => {
-            return (
-                <Bar key={ code } dataKey={ code } fill={ colors[index] } />
-            );
-          })
-      }
-    </BarChart>
-                    )
-                }
+                    { this.props['apiEndpoint'] === 'stats' ?
+                        (
+                            <LineChart data={this.state.stats} margin={{ top: 5, right: 30, left: 20, bottom: 5, }}>
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="date" />
+                                <YAxis />
+                                <Tooltip content={ abc => this.toolTipContent(abc) } />
+                                <Line type="monotone" dataKey="average" stroke="#8884d8" activeDot={{ r: 8 }} />
+                                <Line type="monotone" dataKey="min" stroke="#00ff00" />
+                                <Line type="monotone" dataKey="max" stroke="#ff0000" />
+                            </LineChart>
+                        ) : (
+                            <BarChart data={this.state.stats} margin={{ top: 20, right: 30, left: 20, bottom: 5, }}>
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="date" />
+                                <YAxis />
+                                <Tooltip />
+                                <Legend />
+                                {
+                                    this.state.codes.map((code, index) => {
+                                        return (
+                                            <Bar key={ code } dataKey={ code } fill={ colors[index] } />
+                                        );
+                                    })
+                                }
+                            </BarChart>
+                        )
+                    }
                 </ResponsiveContainer>
             </div>
         );
